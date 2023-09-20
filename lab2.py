8070cd18c399ab97702e4c8aa9c938357f0bd023
70121118 <70121118@uol.pk>
2023-09-20 10:59:47 +0500

    Added avg task in task 1

--- lab2.py
+++ lab2.py
@@ -8,6 +8,13 @@ for i in range(5):
 
 print("Sum is:", sum(list_nos))
 
+avg = 0
+for i in range(5):
+    avg += list_nos[i]
+
+avg = avg / 5
+print("Average is:", avg)
+
 
 # TASK 2
 for i in range(-4, 1):
