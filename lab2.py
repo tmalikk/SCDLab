4f31bfd1d7b6d700100afe04569fe09bf9597986
70121118 <70121118@uol.pk>
2023-09-20 10:06:43 +0500

    Added task 2

--- lab2.py
+++ lab2.py
@@ -1,3 +1,5 @@
+# TASK 1
+
 list_nos = []
 print("Enter list of numbers")
 
@@ -5,3 +7,8 @@ for i in range(5):
     list_nos.append(int(input()))
 
 print("Sum is:", sum(list_nos))
+
+
+# TASK 2
+for i in range(-4, 1):
+    print(list_nos[-i])
