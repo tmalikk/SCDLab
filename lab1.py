40319836bff082efac3104a83f905e751f18db26
tmalikk <145525001+tmalikk@users.noreply.github.com>
2023-09-20 09:23:01 +0500

    Uploading Lab 1 file

---
+++ lab1.py
@@ -0,0 +1,20 @@
+print('"We are learning", he said.')
+
+print('He said, "I\'m learning".')
+
+
+# TASK 2
+radius = 3.12
+pi = 3.14
+
+print(pi * radius ** 2)
+
+
+# TASK 3
+sap_id = input("Write your sap id: ")
+name = input("Write your name: ")
+
+print(name, sap_id)
+
+
+# TASK 4
